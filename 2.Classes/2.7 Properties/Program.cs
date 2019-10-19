e76d979afc6bb235e7f08860a7406b91e74bcc1e
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 00:00:19 +0700

    2_7Finish

--- 2.Classes/2.7 Properties/Program.cs
+++ 2.Classes/2.7 Properties/Program.cs
@@ -10,6 +10,8 @@ namespace Properties
     {
         static void Main(string[] args)
         {
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
         }
     }
 }
