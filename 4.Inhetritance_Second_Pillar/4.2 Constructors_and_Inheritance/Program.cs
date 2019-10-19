f609d57a7c0b1dd0e78a0db2d3e125d5b46a06ef
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 02:04:54 +0700

    4_2Finish

--- 4.Inhetritance_Second_Pillar/4.2 Constructors_and_Inheritance/Program.cs
+++ 4.Inhetritance_Second_Pillar/4.2 Constructors_and_Inheritance/Program.cs
@@ -9,7 +9,8 @@ namespace Constructors
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
+            var car = new Car("XYZ1234");
         }
     }
 }
--- 4.Inhetritance_Second_Pillar/4.2 Constructors_and_Inheritance/Vehicle.cs
+++ 4.Inhetritance_Second_Pillar/4.2 Constructors_and_Inheritance/Vehicle.cs
@@ -5,10 +5,11 @@ namespace Constructors
     public class Vehicle
     {
         private readonly string _registrationNumber;
-        public Vehicle()
-        {
-            Console.WriteLine("Vegicle is being initialized.");
-        }
+//        public Vehicle()
+//        {
+//            Console.WriteLine("Vegicle is being initialized.");
+//        }
+
         public Vehicle(string registrationNumber)
         {
             _registrationNumber = registrationNumber;
