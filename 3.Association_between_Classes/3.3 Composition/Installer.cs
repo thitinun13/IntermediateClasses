ceb22fe3df5c81288949ed19e36bd2a8e7ec9d26
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 00:57:34 +0700

    3_3UpdateInstaller

--- 3.Association_between_Classes/3.3 Composition/Installer.cs
+++ 3.Association_between_Classes/3.3 Composition/Installer.cs
@@ -2,6 +2,14 @@
 {
     public class Installer
     {
-
+        private readonly Logger _logger;
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Install()
+        {
+            _logger.Log("We are installing the cpplication.");
+        }
     }
 }
