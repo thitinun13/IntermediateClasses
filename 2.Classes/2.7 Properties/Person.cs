aaf6f08f48fc4cb2dc92e6d1839ba177e92655ee
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-19 23:57:36 +0700

    2_7UpdatePerson

--- 2.Classes/2.7 Properties/Person.cs
+++ 2.Classes/2.7 Properties/Person.cs
@@ -1,7 +1,25 @@
-﻿namespace Properties
+﻿using System;
+
+namespace Properties
 {
     public class Person
     {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
 
+                return years;
+            }
+        }
     }
 }
