d5179fdeec47f2714ce6097f5f026511853cd8d7
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 00:29:33 +0700

    3_2UpdatePresentationObject

--- 3.Association_between_Classes/3.2 Inheritance/PresentationObject.cs
+++ 3.Association_between_Classes/3.2 Inheritance/PresentationObject.cs
@@ -1,7 +1,19 @@
-﻿namespace Inheritance
+﻿using System;
+
+namespace Inheritance
 {
     public class PresentationObject
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
 
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("Objecy was duplicated.");
+        }
     }
 }
