922936dfcc071415e9030384fd0797e987f88228
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 00:15:41 +0700

    2_8UpdateHittCookie

--- 2.Classes/2.8 Indexers/HittCookie.cs
+++ 2.Classes/2.8 Indexers/HittCookie.cs
@@ -1,7 +1,31 @@
-﻿namespace Indexers
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Indexers
 {
     public class HittCookie
     {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
 
+        public HittCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public void SetItem(string key, string value)
+        {
+
+        }
+        public string GetItem(string key)
+        {
+
+        }
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
     }
 }
