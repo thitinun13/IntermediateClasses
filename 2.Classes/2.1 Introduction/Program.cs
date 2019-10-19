1bc0802496e00bec4fd075f737567a5894d5f2c5
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-19 21:48:27 +0700

    2_1AddClassPerson

--- 2.Classes/2.1 Introduction/Program.cs
+++ 2.Classes/2.1 Introduction/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Introduction
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Mosh");
         }
     }
 }
