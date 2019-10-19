73452c7c221c9530506502252c435db538e03eba
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 01:48:06 +0700

    4_2UpdateVehicle

--- 4.Inhetritance_Second_Pillar/4.2 Constructors_and_Inheritance/Vehicle.cs
+++ 4.Inhetritance_Second_Pillar/4.2 Constructors_and_Inheritance/Vehicle.cs
@@ -1,7 +1,19 @@
-﻿namespace Constructors
+﻿using System;
+
+namespace Constructors
 {
     public class Vehicle
     {
+        private readonly string _registrationNumber;
+        public Vehicle()
+        {
+            Console.WriteLine("Vegicle is being initialized.");
+        }
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
 
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
+        }
     }
 }
