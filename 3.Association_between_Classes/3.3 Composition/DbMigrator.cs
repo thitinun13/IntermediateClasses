75df0a31b1ae2d657a184e674b102060efe10fc8
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 00:53:33 +0700

    3_3UpdateDbMigrator

--- 3.Association_between_Classes/3.3 Composition/DbMigrator.cs
+++ 3.Association_between_Classes/3.3 Composition/DbMigrator.cs
@@ -2,6 +2,14 @@
 {
     public class DbMigrator
     {
-
+        private readonly Logger _logger;
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("We are migrating blah blah blah...");
+        }
     }
 }
