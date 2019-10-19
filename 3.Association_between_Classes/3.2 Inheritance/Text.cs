20a8c5e2b108dfb1e73370e6c32f6e2abf657a4e
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 00:33:12 +0700

    3_2UpdateText

--- 3.Association_between_Classes/3.2 Inheritance/Text.cs
+++ 3.Association_between_Classes/3.2 Inheritance/Text.cs
@@ -1,7 +1,13 @@
 ﻿namespace Inheritance
 {
-    public class Text
+    public class Text : PresentationObject
     {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
 
+        public void AddHyperlink(string url)
+        {
+            System.Console.WriteLine("We added a link to " + url);
+        }
     }
 }
