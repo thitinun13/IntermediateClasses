92e1a1a0ccfd3072e7521f11a2f73a6074f7d9aa
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-19 22:50:17 +0700

    2_4UpdateCalculator

--- 2.Classes/2.4 Methods/Calculator.cs
+++ 2.Classes/2.4 Methods/Calculator.cs
@@ -2,6 +2,14 @@
 {
     public class Calculator
     {
-
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
     }
 }
