7c6395db9da9ecf37716754c8d8c85f2769f73a3
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-19 22:17:48 +0700

    2_2Finish

--- 2.Classes/2.2 Constructors/Program.cs
+++ 2.Classes/2.2 Constructors/Program.cs
@@ -10,6 +10,15 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
