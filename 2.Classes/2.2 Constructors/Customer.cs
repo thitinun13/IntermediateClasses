1efa52b1e3778eb4a3b73cc8a2e299cf4bde25d4
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-19 22:11:53 +0700

    2_2UpdateCustomer

--- 2.Classes/2.2 Constructors/Customer.cs
+++ 2.Classes/2.2 Constructors/Customer.cs
@@ -1,7 +1,28 @@
-﻿namespace Constructors
+﻿using System.Collections.Generic;
+
+namespace Constructors
 {
     public class Customer
     {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
 
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+
+        }
     }
 }
