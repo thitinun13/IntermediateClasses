65ccc37baaf2663af6eb881f73eed5cba87808ed
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 01:02:21 +0700

    3_3Finish

--- 3.Association_between_Classes/3.3 Composition/Program.cs
+++ 3.Association_between_Classes/3.3 Composition/Program.cs
@@ -10,6 +10,13 @@ namespace Composition
     {
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new Logger());
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
 }
