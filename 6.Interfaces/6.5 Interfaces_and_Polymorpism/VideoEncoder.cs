af765ca3dc5aa84deafa42f4599dd32bbb224d8d
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 20:38:35 +0700

    6_5UpdateVideoEncoder

--- 6.Interfaces/6.5 Interfaces_and_Polymorpism/VideoEncoder.cs
+++ 6.Interfaces/6.5 Interfaces_and_Polymorpism/VideoEncoder.cs
@@ -1,7 +1,24 @@
-﻿namespace InterfacePolymor
+﻿using System.Collections.Generic;
+
+namespace InterfacePolymor
 {
     public class VideoEncoder
     {
-
+        private readonly IList<INotificationChannel> _notificationChannels;
+        public VideoEncoder()
+        {
+            _notificationChannels = new List<INotificationChannel>();
+        }
+        public void Encode(Video video)
+        {
+            // Video encoding logic
+            // ...
+            foreach (var channel in _notificationChannels)
+                channel.Send(new Message());
+        }
+        public void RegisterNotificationChannel(INotificationChannel channel)
+        {
+            _notificationChannels.Add(channel);
+        }
     }
 }
