a7f5fb3e48b00de050349100731b74581e9f9859
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 18:00:42 +0700

    6_3UpdateDbMigrator

--- 6.Interfaces/6.3 Interfaces_and_Extensibility/DbMigrator.cs
+++ 6.Interfaces/6.3 Interfaces_and_Extensibility/DbMigrator.cs
@@ -1,7 +1,21 @@
-﻿namespace Extensiblity
+﻿using System;
+
+namespace Extensiblity
 {
     public class DbMigrator
     {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
 
+            // Details of migrating the datebase
+
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+        }
     }
 }
