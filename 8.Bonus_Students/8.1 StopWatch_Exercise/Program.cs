672f63f94153189c617a1f2e46280831ab95653f
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 20:56:07 +0700

    8_1Update

--- 8.Bonus_Students/8.1 StopWatch_Exercise/Program.cs
+++ 8.Bonus_Students/8.1 StopWatch_Exercise/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StopWatch
@@ -10,6 +11,55 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
+            var stopwatch = new Stopwatch();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stopwatch.start(DateTime.Now);
+
+                for (int j = 0; j < 1000; j++)
+                {
+                    Thread.Sleep(1);
+                }
+
+                stopwatch.stop(DateTime.Now);
+                Console.WriteLine(stopwatch.GetInterval().ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        private bool running = false;
+
+        public void start(DateTime start)
+        {
+            if (!running)
+            {
+                StartTime = start;
+                running = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is already running");
+            }
+        }
+        public void stop(DateTime stop)
+        {
+            if (running)
+            {
+                EndTime = stop;
+                running = false;
+            }
+        }
+
+        public TimeSpan GetInterval()
+        {
+            var duration = EndTime - StartTime;
+            return duration;
         }
     }
 }
