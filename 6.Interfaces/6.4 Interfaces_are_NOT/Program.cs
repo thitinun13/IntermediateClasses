c995f65df81c4a924af04b853aa1f2a2e07ee625
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 19:57:06 +0700

    6_4CreateFile

---
+++ 6.Interfaces/6.4 Interfaces_are_NOT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAndInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
