f8a30411956b285f36b4bbe65a0a2858a2f04a69
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 18:08:56 +0700

    6_3UpdateFileLogger

--- 6.Interfaces/6.3 Interfaces_and_Extensibility/FileLogger.cs
+++ 6.Interfaces/6.3 Interfaces_and_Extensibility/FileLogger.cs
@@ -1,7 +1,30 @@
-﻿namespace Extensiblity
-{
-    public class FileLogger
-    {
+﻿using System.IO;
 
+namespace Extensiblity
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+
+        public void LogError(string message)
+        {
+            Log(message, "ERROR");
+        }
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+        }
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
     }
 }
