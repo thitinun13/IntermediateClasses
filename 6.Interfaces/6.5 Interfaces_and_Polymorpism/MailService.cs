cf0d433dd661e3f817b54332aebc2bdc63315d31
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 20:28:18 +0700

    6_5UpdateMailService

--- 6.Interfaces/6.5 Interfaces_and_Polymorpism/MailService.cs
+++ 6.Interfaces/6.5 Interfaces_and_Polymorpism/MailService.cs
@@ -1,7 +1,12 @@
-﻿namespace InterfacePolymor
+﻿using System;
+
+namespace InterfacePolymor
 {
     public class MailService
     {
-
+        public void Send(Mail mail)
+        {
+            Console.WriteLine("Sending email...");
+        }
     }
 }
