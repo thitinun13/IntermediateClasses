df6c3d8724966b0ce44d6dfe4d6b86965c59f8d6
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 16:06:53 +0700

    5_1Finish

--- 5.Polymorphism_Third_Pillar/5.1 Method_Overriding/Program.cs
+++ 5.Polymorphism_Third_Pillar/5.1 Method_Overriding/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace MethodOverriding
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
         }
     }
 }
