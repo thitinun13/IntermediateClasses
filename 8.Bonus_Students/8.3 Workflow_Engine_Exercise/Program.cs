6b60dbf49a82f3019265136613a94b0ff1b35b22
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 21:49:59 +0700

    8_3Update

--- 8.Bonus_Students/8.3 Workflow_Engine_Exercise/Program.cs
+++ 8.Bonus_Students/8.3 Workflow_Engine_Exercise/Program.cs
@@ -10,6 +10,72 @@ namespace Workflow
     {
         static void Main(string[] args)
         {
+            WorkFlowEngine workFlow = new WorkFlowEngine();
+            workFlow.AddWorkFlowObject(new VideoUploader());
+            workFlow.AddWorkFlowObject(new CallWebService());
+            workFlow.AddWorkFlowObject(new SendEmail());
+            workFlow.AddWorkFlowObject(new ChangeStatus());
+
+            workFlow.Run();
+
+            Console.ReadLine();
+        }
+    }
+    public interface IWorkFlow
+    {
+        void Execute();
+    }
+    class VideoUploader : IWorkFlow
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Uploading a Video");
+        }
+    }
+    class CallWebService : IWorkFlow
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Calling Web Service");
+        }
+    }
+    class SendEmail : IWorkFlow
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sending Email");
+        }
+    }
+    class ChangeStatus : IWorkFlow
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Status Changed");
+        }
+    }
+    class WorkFlowEngine
+    {
+        private List<IWorkFlow> T;
+
+        public WorkFlowEngine()
+        {
+            T = new List<IWorkFlow>();
+        }
+
+        public void AddWorkFlowObject(IWorkFlow iObject)
+        {
+            T.Add(iObject);
+        }
+        public void RemoveWorkFlowObject(IWorkFlow iObject)
+        {
+            T.Remove(iObject);
+        }
+        public void Run()
+        {
+            foreach (IWorkFlow I in T)
+            {
+                I.Execute();
+            }
         }
     }
 }
