5cfc57fbe9f3e0112a79512f1b3ae8801d86ad82
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 17:24:59 +0700

    6_2UpdateMain

--- 6.Interfaces/6.2 Interfaces_and_Testabillity/Program.cs
+++ 6.Interfaces/6.2 Interfaces_and_Testabillity/Program.cs
@@ -10,6 +10,9 @@ namespace Testability
     {
         static void Main(string[] args)
         {
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
         }
     }
 }
