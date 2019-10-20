95ca4564ddbebff9a97500999cdc2475ef3ee415
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 21:12:05 +0700

    8_1Finish

--- 8.Bonus_Students/8.1 StopWatch_Exercise/Program.cs
+++ 8.Bonus_Students/8.1 StopWatch_Exercise/Program.cs
@@ -15,22 +15,22 @@ namespace StopWatch
 
             for (int i = 0; i < 2; i++)
             {
-                stopwatch.start(DateTime.Today.AddDays(1));
+                stopwatch.start();
 
-                for (int j = 0; j < 1000; j++)
-                {
-                    Thread.Sleep(1);
-                }
+                Thread.Sleep(1000);
 
-                stopwatch.stop(DateTime.Today.AddYears(-1));
-                Console.WriteLine(stopwatch.GetInterval().ToString());
+                stopwatch.stop();
+
+                Console.WriteLine("Duration : " + stopwatch.GetInterval().ToString());
+
+                Console.WriteLine("Press Enter to run the stopwatch one more time");
                 Console.ReadLine();
             }
         }
     }
     public class Stopwatch
     {
-        private DateTime _StartTime;
+        private DateTime _startTime;
         private DateTime _endTime;
 
         private bool _running;
