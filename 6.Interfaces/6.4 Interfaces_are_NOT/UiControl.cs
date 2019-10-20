f7d2d04bafa4b68ac607d1dbf7e4bf00fd74bff7
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 20:14:04 +0700

    6_4Finish

--- 6.Interfaces/6.4 Interfaces_are_NOT/UiControl.cs
+++ 6.Interfaces/6.4 Interfaces_are_NOT/UiControl.cs
@@ -2,6 +2,17 @@
 
 namespace InterfaceAndInheritance
 {
+    public class TextBox : UiControl, IDraggable, IDroppable
+    {
+        public void Drag()
+        {
+            throw new NotImplementedException();
+        }
+        public void Drop()
+        {
+            throw new NotImplementedException();
+        }
+    }
     public class UiControl
     {
         public string Id { get; set; }
