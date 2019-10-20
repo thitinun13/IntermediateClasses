26428bc6d5ba7a4cd0677efab0dce5054bd65deb
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 16:42:45 +0700

    5_2Finish

--- 5.Polymorphism_Third_Pillar/5.2 Abstract_Classes_and_Members/Program.cs
+++ 5.Polymorphism_Third_Pillar/5.2 Abstract_Classes_and_Members/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Abstract
 {
-    partial class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
         }
     }
 }
