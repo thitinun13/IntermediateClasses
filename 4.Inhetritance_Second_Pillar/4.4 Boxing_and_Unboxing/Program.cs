02737d1ed691749d4c8b41b70ec2c83bfaa2fd63
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 15:25:42 +0700

    4_4Finish

--- 4.Inhetritance_Second_Pillar/4.4 Boxing_and_Unboxing/Program.cs
+++ 4.Inhetritance_Second_Pillar/4.4 Boxing_and_Unboxing/Program.cs
@@ -10,6 +10,14 @@ namespace Boxing
     {
         static void Main(string[] args)
         {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Mosh");
+            list.Add(DateTime.Today);
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add();
         }
     }
 }
