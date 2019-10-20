2c9a400878d405805af91c5fa01c8be5496dc0a6
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 18:14:46 +0700

    6_3UpdateConsoleLogger

--- 6.Interfaces/6.3 Interfaces_and_Extensibility/ConsoleLogger.cs
+++ 6.Interfaces/6.3 Interfaces_and_Extensibility/ConsoleLogger.cs
@@ -1,7 +1,18 @@
-﻿namespace Extensiblity
-{
-    public class ConsoleLogger
-    {
+﻿using System;
 
+namespace Extensiblity
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
     }
 }
