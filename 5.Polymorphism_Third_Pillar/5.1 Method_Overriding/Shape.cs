e65698876c268980728f401deeded7e13e0ef89d
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-20 15:41:33 +0700

    5_1UpdateShape

--- 5.Polymorphism_Third_Pillar/5.1 Method_Overriding/Shape.cs
+++ 5.Polymorphism_Third_Pillar/5.1 Method_Overriding/Shape.cs
@@ -2,6 +2,13 @@
 {
     public class Shape
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
 
+        public virtual void Draw()
+        {
+
+        }
     }
 }
